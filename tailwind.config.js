a98ee2eb9f9419b5b94dabfe699d11b4bb37416c
Arnab Munshi <arnab47stars@gmail.com>
2022-06-05 17:19:19 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    './layouts/*.html'
+  ],
+  theme: {
+    letterSpacing: {
+      'base': '1.2px',
+    },
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      fontSize: {
+        'base': '12px',
+      }
+    },
+  },
+  plugins: [],
+}
